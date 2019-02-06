8535a90468cb4320bdfbf63f4888bed21ec787e7
gauravdal <gaurav.bamania@dal.ca>
2019-02-06 13:06:54 -0400

    second update

--- openbook1.py
+++ openbook1.py
@@ -1,5 +1,16 @@
 
-def list_ifname_ip(config_file):
-	config_dict = []
-	fout = open(config_file,"r"):
-	
+def list_ifname_ip():
+	config_key = {}
+	fout = open("running-config.cfg" ,"r")
+	for line in fout:
+		if("nameif" in line):
+			temp_key = line.split()
+			if("nameif"!= temp_key[1]):
+				print(temp_key[1])
+	fout.seek(0)
+	for line in fout:
+		if("ip address" in line):
+			temp_ip = line.split()
+			print(temp_ip)
+
+list_ifname_ip()
