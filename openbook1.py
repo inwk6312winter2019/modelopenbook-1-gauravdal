b5611b82110e96930b10b6918835f30396d7e2eb
gauravdal <gaurav.bamania@dal.ca>
2019-02-06 13:20:49 -0400

    third update

--- openbook1.py
+++ openbook1.py
@@ -1,16 +1,18 @@
 
 def list_ifname_ip():
 	config_key = {}
+	key = []
+	ip_addr = []
 	fout = open("running-config.cfg" ,"r")
 	for line in fout:
 		if("nameif" in line):
 			temp_key = line.split()
 			if("nameif"!= temp_key[1]):
 				print(temp_key[1])
-	fout.seek(0)
-	for line in fout:
 		if("ip address" in line):
 			temp_ip = line.split()
-			print(temp_ip)
+			if("ip"== temp_ip[0]):
+				print(temp_ip[2:])
+	config_key
 
 list_ifname_ip()
